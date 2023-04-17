daba1a9af2c70e39aa60763d008510144342d9cc
Václav Brožík <5073939+vbrozik@users.noreply.github.com>
2023-04-17 09:28:34 +0200

    add(regex): Add re_ext module for regexes

--- src/vb/utils/__init__.py
+++ src/vb/utils/__init__.py
@@ -5,6 +5,6 @@ The module contains utility functions for project by Václav Brožík.
 
 from __future__ import annotations
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 # __all__ = []
---
+++ src/vb/utils/re_ext.py
@@ -0,0 +1,28 @@
+"""Extend standard re module with some useful features."""
+
+from __future__ import annotations
+
+import re
+
+
+def first_fullmatch(
+        patterns: list[re.Pattern[str] | str], string: str) -> re.Match[str] | None:
+    """Return the first fullmatch of a string.
+
+    Args:
+        string: string to match
+        regexes: list of compiled regular expressions
+
+    Returns:
+        The first fullmatch of the string or None if no match is found.
+
+    Examples:
+        >>> first_fullmatch('foo', [re.compile('foo'), re.compile('bar')])
+        <re.Match object; span=(0, 3), match='foo'>
+
+        >>> first_fullmatch('baz', [re.compile('bar'), re.compile('foo')])
+    """
+    for pattern in patterns:
+        if match := re.fullmatch(pattern, string):
+            return match
+    return None
